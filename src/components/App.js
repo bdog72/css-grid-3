b9c3cbefcdbd34c141e18d69cc9e21e86a5e7393
Brian Bycynski <high5bri@hotmail.com>
2017-03-20 12:13:22 -0400

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
+    return <div className='wrapper'>
+      <div className='box a'>A</div>
+      <div className='box b'>B</div>
+      <div className='box c'>C</div>
+      <div className='box d'>D</div>
+      <div className='box e'>E</div>
+      <div className='box f'>F</div>
     </div>
   }
 }
